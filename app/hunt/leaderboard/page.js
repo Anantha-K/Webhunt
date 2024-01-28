40bced8840c238ba5c5a459e99976c0fa8bed8dd
Anantha Krishnan <jananthak@gmail.com>
2024-01-28 22:45:18 +0530

    leaderboard

---
+++ app/hunt/leaderboard/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+<div className='w-full flex bg-black'>
+  <section className='bg-red-400 h-screen w-1/6'>
+
+  </section>
+<section className='bg-blue-200 w-4/6 h-screen'>
+
+</section>
+<section  className='bg-yellow-500 w-1/6 h-screen'>
+
+</section>
+
+</div>  )
+}
+
+export default page
