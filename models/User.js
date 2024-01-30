3b52493c68193a00c778c56de01413a4bb02b5a1
Anantha Krishnan <jananthak@gmail.com>
2024-01-30 21:45:32 +0530

    ui

--- models/User.js
+++ models/User.js
@@ -14,7 +14,6 @@ const userSchema = mongoose.Schema({
 },{timestamps:true}
 )
 
-const User = mongoose.models.User || mongoose.model("User",userSchema)
+export default mongoose.models.User || mongoose.model("User",userSchema)
 
 
-export default User;
